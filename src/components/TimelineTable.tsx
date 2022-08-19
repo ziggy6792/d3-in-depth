bd136d97adf6abd3746f4143c1de655e137547a6
Simon Verhoeven <simon.verhoeven@telepathy.ai>
2022-08-19 11:53:54 +0800

    change to tss api

--- src/components/TimelineTable.tsx
+++ src/components/TimelineTable.tsx
@@ -2,6 +2,7 @@ import { FC, memo, useMemo, useState } from 'react';
 import { alpha, Box, Grid, styled, Typography } from '@mui/material';
 
 import { NodeInterface } from 'src/components/ForceGraph';
+import { makeStyles } from 'src/makeStyles';
 
 type SegmentRowProp = {
   playing: boolean;
@@ -9,30 +10,13 @@ type SegmentRowProp = {
   isClickable?: boolean;
 };
 
-const StyledColumn = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.common.darkCloudyBlue,
-  backgroundBlendMode: 'multiply',
-  borderRadius: theme.spacing(1),
-  zIndex: 0,
-}));
-
-const StyledRow = styled(Box, {
-  shouldForwardProp: (prop) => !['playing', 'showHighlight', 'isClickable'].includes(prop.toString()),
-})<SegmentRowProp>(({ theme, showHighlight, playing, isClickable }) => ({
-  backgroundColor: showHighlight ? theme.palette.common.darkCloudyBlue : undefined,
-  borderRadius: theme.spacing(1),
-  backgroundBlendMode: 'multiply',
-  zIndex: 0,
-  marginLeft: theme.spacing(1),
-  marginRight: theme.spacing(1),
-  border: playing ? '3px solid' : '',
-  borderColor: playing ? theme.palette.primary.main : '',
-  '&:hover': isClickable
-    ? {
-        color: theme.palette.common.bhaBlue,
-        backgroundColor: alpha(theme.palette.primary.main, 0.5),
-      }
-    : undefined,
+const useTableStyles = makeStyles()((theme) => ({
+  boxBase: {
+    backgroundColor: theme.palette.common.darkCloudyBlue,
+    backgroundBlendMode: 'multiply',
+    borderRadius: theme.spacing(1),
+    zIndex: 0,
+  },
 }));
 
 // ToDo pass as props
@@ -59,12 +43,57 @@ const TimelineTable: FC<ITimelineProps> = ({ timelineEvents, rowHeight = '1fr' }
   );
 };
 
+interface TimelineTableRowProps extends ITimelineBaseProps {
+  event: NodeInterface;
+  index: number;
+  gridTemplateColumns: string;
+}
+
+const TimelineTableRow: FC<TimelineTableRowProps> = ({ event, index, gridTemplateColumns }) => {
+  const { classes: rowClasses } = useRowStyles({ showHighlight: index % 2 === 0, playing: false, isClickable: true });
+
+  return (
+    <Box className={rowClasses.tableRow} key={event.id} gridColumn='1/ -1' gridRow={index + 2} onClick={() => null}>
+      <Box display='grid' gridTemplateColumns={gridTemplateColumns}>
+        <Box padding={1}>
+          <Typography color='white'>bla </Typography>
+        </Box>
+        <Box padding={1}>
+          <Typography color='white'>bla </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
 interface ISegmentDetailProps extends ITimelineBaseProps {
   timelineEvents: NodeInterface[];
 }
 
+const useRowStyles = makeStyles<SegmentRowProp>()((theme, { showHighlight, playing, isClickable }) => ({
+  tableRow: {
+    backgroundColor: showHighlight ? theme.palette.common.darkCloudyBlue : undefined,
+    borderRadius: theme.spacing(1),
+    backgroundBlendMode: 'multiply',
+    zIndex: 0,
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    border: playing ? '3px solid' : '',
+    borderColor: playing ? theme.palette.primary.main : '',
+    '&:hover': isClickable
+      ? {
+          color: theme.palette.common.bhaBlue,
+          backgroundColor: alpha(theme.palette.primary.main, 0.5),
+        }
+      : undefined,
+  },
+}));
+
 const TimelineTableRows: FC<ISegmentDetailProps> = ({ timelineEvents, rowHeight }) => {
   const gridTemplateColumns = useMemo(() => columns.map(({ template }) => template).join(' '), []);
+
+  const { classes } = useTableStyles();
+  const { classes: rowClasses } = useRowStyles({ showHighlight: false, playing: false, isClickable: false });
 
   return (
     <Box sx={{ width: 1 }}>
@@ -77,10 +106,10 @@ const TimelineTableRows: FC<ISegmentDetailProps> = ({ timelineEvents, rowHeight 
       >
         {/* Column highlights */}
         {columns.map(({ name }, index) => (
-          <StyledColumn key={name} gridColumn={index + 1} gridRow='1/ -1' padding={1}></StyledColumn>
+          <Box className={classes.boxBase} key={name} gridColumn={index + 1} gridRow='1/ -1' padding={1}></Box>
         ))}
 
-        <StyledRow gridColumn='1/ -1' gridRow={1} showHighlight={false} playing={false}>
+        <Box className={rowClasses.tableRow} gridColumn='1/ -1' gridRow={1}>
           <Box display='grid' gridTemplateColumns={gridTemplateColumns}>
             {columns.map(({ name }) => (
               <Box key={name} padding={1}>
@@ -88,27 +117,28 @@ const TimelineTableRows: FC<ISegmentDetailProps> = ({ timelineEvents, rowHeight 
               </Box>
             ))}
           </Box>
-        </StyledRow>
+        </Box>
 
         {timelineEvents.map((timelineEvent, index) => (
-          <StyledRow
-            isClickable
-            key={timelineEvent.id}
-            gridColumn='1/ -1'
-            gridRow={index + 2}
-            showHighlight={index % 2 === 0}
-            playing={false}
-            onClick={() => null}
-          >
-            <Box display='grid' gridTemplateColumns={gridTemplateColumns}>
-              <Box padding={1}>
-                <Typography color='white'>{timelineEvent.details.name} </Typography>
-              </Box>
-              <Box padding={1}>
-                <Typography color='white'>{timelineEvent.details.name} </Typography>
-              </Box>
-            </Box>
-          </StyledRow>
+          <TimelineTableRow index={index} key={timelineEvent.id} event={timelineEvent} gridTemplateColumns={gridTemplateColumns} />
+          // <StyledRow
+          //   isClickable
+          //   key={timelineEvent.id}
+          //   gridColumn='1/ -1'
+          //   gridRow={index + 2}
+          //   showHighlight={index % 2 === 0}
+          //   playing={false}
+          //   onClick={() => null}
+          // >
+          //   <Box display='grid' gridTemplateColumns={gridTemplateColumns}>
+          //     <Box padding={1}>
+          //       <Typography color='white'>{timelineEvent.details.name} </Typography>
+          //     </Box>
+          //     <Box padding={1}>
+          //       <Typography color='white'>{timelineEvent.details.name} </Typography>
+          //     </Box>
+          //   </Box>
+          // </StyledRow>
         ))}
       </Box>
     </Box>
