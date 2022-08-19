5c495628a8ac8e7e1646c20a4e9b722627495078
Simon Verhoeven <simon.verhoeven@telepathy.ai>
2022-08-19 17:47:39 +0800

    cleanup

--- src/components/Simulation/SimulationTimeline.tsx
+++ src/components/Simulation/SimulationTimeline.tsx
@@ -1,12 +1,33 @@
 import { Box, Typography } from '@mui/material';
 import { NodeInterface } from 'src/components/ForceGraph/types';
 import TimelineTable from 'src/components/TimelineTable';
+import { useDispatchSimulationContext, useSimulationContext } from './SimulaitionProvider';
 
 interface ISimulationTimelineProps {
   events: NodeInterface[];
 }
 
+interface ISimulationTimelineRowProps {
+  node: NodeInterface;
+}
+
+const SimulationTimelineRow: React.FC<ISimulationTimelineRowProps> = ({ node }) => {
+  return (
+    <>
+      <Box padding={1}>
+        <Typography color='white'>{node.id}</Typography>
+      </Box>
+      <Box padding={1}>
+        <Typography color='white'>{node.details.name}</Typography>
+      </Box>
+    </>
+  );
+};
+
 const SimulationTimeline: React.FC<ISimulationTimelineProps> = ({ events }) => {
+  const { dispatch } = useDispatchSimulationContext();
+  const { time } = useSimulationContext();
+
   return (
     <TimelineTable
       columns={[
@@ -14,16 +35,8 @@ const SimulationTimeline: React.FC<ISimulationTimelineProps> = ({ events }) => {
         { name: 'Name', template: '2fr' },
       ]}
       rows={events}
-      renderRow={(node) => (
-        <>
-          <Box padding={1}>
-            <Typography color='white'>{node.id}</Typography>
-          </Box>
-          <Box padding={1}>
-            <Typography color='white'>{node.details.name}</Typography>
-          </Box>
-        </>
-      )}
+      // onRowSelected={(row) => dispatch({type: 'setTime', payload: row.details})}
+      renderRow={(node) => <SimulationTimelineRow node={node} />}
     />
   );
 };
